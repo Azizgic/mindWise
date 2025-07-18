91ff1095c13842d594b5ec027ecc208caa2d3cfb
Azizgic <azizgichki3@gmail.com>
2025-07-19 02:13:49 +0500

    added a ThemeProvider

--- App.tsx
+++ App.tsx
@@ -3,18 +3,24 @@ import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import CategoryScreen from "./screens/CategoryScreen";
 import FlashcardScreen from "./screens/FlashcardScreen";
+import { ThemeProvider } from "./ThemeProvider";
 
 export default function App() {
   const [category, setCategory] = useState<string | null>(null);
 
   return (
-    <View style={styles.appContainer}>
-      {category ? (
-        <FlashcardScreen category={category} onBack={() => setCategory(null)} />
-      ) : (
-        <CategoryScreen onSelectCategory={(cat) => setCategory(cat)} />
-      )}
-    </View>
+    <ThemeProvider>
+      <View style={styles.appContainer}>
+        {category ? (
+          <FlashcardScreen
+            category={category}
+            onBack={() => setCategory(null)}
+          />
+        ) : (
+          <CategoryScreen onSelectCategory={(cat) => setCategory(cat)} />
+        )}
+      </View>
+    </ThemeProvider>
   );
 }
 
---
+++ ThemeProvider.tsx
@@ -0,0 +1,53 @@
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+import { useColorScheme } from "react-native";
+
+type ThemeType = "dark" | "light";
+
+interface ThemeContextType {
+  theme: ThemeType;
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | null>(null);
+
+export function useTheme(): ThemeContextType {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error("useTheme must be used wothin a ThemeProvid");
+  }
+
+  return context;
+}
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+export function ThemeProvider({ children }: ThemeProviderProps) {
+  const systemColorScheme = useColorScheme();
+  const [theme, setTheme] = useState<ThemeType>(systemColorScheme || "light");
+
+  // Sync with system themee
+  useEffect(() => {
+    if (systemColorScheme) {
+      setTheme(systemColorScheme);
+    }
+  }, [systemColorScheme]);
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  };
+
+  const value = useMemo(() => ({ theme, toggleTheme }), [theme]);
+
+  return (
+    <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+  );
+}
